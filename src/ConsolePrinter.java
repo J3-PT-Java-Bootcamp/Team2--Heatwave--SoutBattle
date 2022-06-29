5e1f36537957ecc677e4a8b9c8c0eacaf62da399
VladScv <ddcllrnsbrv@uoc.edu>
2022-06-29 15:40:33 +0200

    New Printer Class (on process)

--- src/ConsolePrinter.java
+++ src/ConsolePrinter.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.io.*;
 
 public class ConsolePrinter {
@@ -23,16 +22,15 @@ public class ConsolePrinter {
 \\____/ \\____/ \\____//_/  /_/   \\___/ \\__,_//_/ /_/ /_/"""; //TEAM NAME LOGO
     private final PrintStream out;
     private final BufferedReader in;
-    private final StringBuilder strBuilder;
 
-    //---------------------------------------------------------------------------CONSTRUCTOR
+
+    //---------------------------------------------------------------------------   CONSTRUCTOR
     public ConsolePrinter() {
         this.out = System.out;
         this.in= new BufferedReader(new InputStreamReader(System.in));
-        this.strBuilder= new StringBuilder();
     }
 
-    //---------------------------------------------------------------------------PUBLIC METHODS
+    //---------------------------------------------------------------------------   PUBLIC METHODS
     /**Shows the Team Logo after calibrating console size
      */
     public void splashScreen(){
@@ -41,8 +39,24 @@ public class ConsolePrinter {
         out.println(centerText(GAME_LOGO));
 
     }
+    public void printError(String message){
+        //TODO prints a error message
+    }
+    public void printChooseCharacter(Party party){
+        //TODO prints all characters of a party and all the stats and waits until user choose one
+        out.println(partyToString(party));
+    }
+    public void printWinnerCharacter(Character fighter){
+        //TODO print message to announce current fight winner
+    }
+    public void printGameOver(Boolean playerWins){
+        //TODO print game over screen (model depends on player winning or not)
+    }
+    public void printMemorial(String[] graveyard){
+        //TODO prints memorial screen with all dead fighters
+    }
 
-    //---------------------------------------------------------------------------TEXT MANIPULATION
+    //---------------------------------------------------------------------------   TEXT MANIPULATION
     /**Transform a Multiline Text to be shown in the center
      * of our screen by adding spaces both sides
      * @param text String to center on Screen
@@ -51,14 +65,14 @@ public class ConsolePrinter {
     private String centerText(String text){
         var splitText = splitText(text);
         int leftSpace,rightSpace,remainSpace;
-        String resVal="";
+        StringBuilder resVal= new StringBuilder();
         for (String s : splitText) {
             remainSpace = LIMIT_X - s.length();
             leftSpace = remainSpace / 2;
             rightSpace = (remainSpace % 2 == 0) ? leftSpace : leftSpace + 1;
-            resVal += " ".repeat(leftSpace) + s + " ".repeat(rightSpace) + "\n";
+            resVal.append(" ".repeat(leftSpace)).append(s).append(" ".repeat(rightSpace)).append("\n");
         }
-        return resVal;
+        return resVal.toString();
     }
 
 
@@ -66,15 +80,15 @@ public class ConsolePrinter {
      * @param mustAdd True for adding tabulations, False to trim each line
      * @param numTabulations    Number of tabulations to add (each tabulation = TAB_INDENT spaces)
      * @param text  Text to be tabulated
-     * @return  Formated text
+     * @return  Formatted text
      */
     private String changeTab(boolean mustAdd, int numTabulations,String text){
         var auxList= splitText(text);
-        text="";
+        StringBuilder textBuilder = new StringBuilder();
         for (String s : auxList) {
-            text += (mustAdd ? (" ".repeat(TAB_INDENT)).repeat(numTabulations) + s : s.trim()) + "\n";
+            textBuilder.append(mustAdd ? (" ".repeat(TAB_INDENT)).repeat(numTabulations) + s : s.trim()).append("\n");
         }
-        return text;
+        return textBuilder.toString();
     }
 
     /** Splits a multi line string into array of String lines
@@ -84,20 +98,31 @@ public class ConsolePrinter {
     private String[] splitText(String text){
         return text.split("\n");
     }
-
     /** Join a String Array into only one String, separated by new line
      * @param splitText String Array to join
      * @return String with all array members joined, each for line
      */
     private String joinText(String[] splitText){
-        String resVal="";
+        StringBuilder resVal= new StringBuilder();
         for (String s : splitText) {
-            resVal += s + "\n";
+            resVal.append(s).append("\n");
         }
-        return resVal;
+        return resVal.toString();
     }
 
-    //---------------------------------------------------------------------------CONSOLE MANAGER
+    /**Adds new lines to show specified text at console Top (aprox)
+     * @param text to show on top
+     * @return formatted text
+     */
+    private String textToTop(String text){
+        return  text+="\n".repeat(LIMIT_Y-text.split("\n").length);
+    }
+    private String partyToString(Party party){
+        return "";
+    }
+
+
+    //---------------------------------------------------------------------------   CONSOLE MANAGER
     /** Shows Square with the screen size to allow User to resize console,
      *  waits until user confirm
      */
@@ -111,7 +136,13 @@ public class ConsolePrinter {
         }
     }
 
+    /** Sends new lines to fill screen and clear last output
+     */
+    private void clearScreen(){
+        out.println("\n".repeat(LIMIT_Y));
+    }
 
 
-
+    private class Party {//TODO as separated class, only here while not implemented
+    }
 }
