90086480c9cf0453861a71db6a4a8cc7842179a4
BeaIbanez <bea.ibanez.perez@gmail.com>
2022-07-11 15:52:33 +0200

    4d commit Bea

---
+++ src/main/java/Characters/Party.java
@@ -0,0 +1,40 @@
+package Characters;
+
+import java.util.ArrayList;
+
+public class Party {
+    private ArrayList<Character> characterList;
+    private final int MAX_FIGHTERS = 5 ;
+
+    public Party() {
+        characterList= new ArrayList<>();
+        while(!isFull()){
+            addCharacter(getRandomCharacter());
+
+        }
+    }
+
+    private Character getRandomCharacter() {
+
+        return null; //TODO
+        //CREAR WARRIOR O WIZARD , RANDOM
+    }
+
+
+    public ArrayList<Character> getCharacterList() {
+        return characterList;
+    }
+
+    public void addCharacter (Character fighter) {
+       
+        if (isFull())
+        {
+            characterList.add(fighter);
+        }
+        
+    }
+
+    public boolean isFull() {
+        return characterList.size() < MAX_FIGHTERS;
+    }
+}
